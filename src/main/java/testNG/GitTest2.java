0e71e239f253ba4da593be350aeea4dca04d1571
Frederick Kemeh <fkemeh08@gmail.com>
2021-09-28 00:04:02 -0400

    4th commit

--- src/main/java/testNG/GitTest2.java
+++ src/main/java/testNG/GitTest2.java
@@ -4,6 +4,7 @@ public class GitTest2 {
 	public static void main(String[] args) {
 		printMe();
 		printMe2();
+		printMe3();
 	}
 	
 	public static void printMe() {
@@ -11,6 +12,10 @@ public class GitTest2 {
 	}
 	
 	public static void printMe2() {
+		System.out.println("git test");
+	}
+	
+	public static void printMe3() {
 		System.out.println("git test");
 	}
 	
